646734225c52136f4fb095696345eee0edd6c83f
Mario Córdoba <cord0388@gmail.com>
2024-03-30 20:52:43 -0300

    add all three resolutions

--- api/index.js
+++ api/index.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const app = express();
+const fs = require("fs");
+const dataPath = './data/data.json';
 
-app.get("/express", (req, res) => res.send("Express on Vercel!"));
-app.get("/cliente_servidor", (req, res) => res.send("Cliente Servidor on Vercel!"));
+app.get("/express", (req, res) => {
+    fs.readFile(dataPath, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Server Error');
+        } else {
+            res.json(JSON.parse(data));
+        }
+    });
+});
+//app.get("/cliente_servidor", (req, res) => res.send("Cliente Servidor on Vercel!"));
 app.use(express.static('public'))
 
 
---
+++ public/cliente_servidor/script.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const tableBody = document.getElementById('table-body');
+
+    // Fetch JSON data
+    fetch("http://localhost:3001/express")
+        .then(response => response.json())
+        .then(data => {
+            // Clear existing table rows
+            tableBody.innerHTML = '';
+
+            // Iterate through JSON data and create table rows
+            data.forEach(item => {
+                const row = document.createElement('tr');
+                row.innerHTML = `
+                    <th scope="row">${item.id}</th>
+                    <td>${item.first_name}</td>
+                    <td>${item.last_name}</td>
+                    <td>${item.email}</td>
+                    <td>${item.birthdate}</td>
+                    <td>${item.credit_card}</td>
+                `;
+                tableBody.appendChild(row);
+            });
+        })
+        .catch(error => console.error('Error fetching JSON:', error));
+});
