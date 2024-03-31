2fd67b65d4a6024ce647cd3a9f6b7a322cc4a9f6
Mario Córdoba <cord0388@gmail.com>
2024-03-31 11:10:52 -0300

    more corrections

--- api/index.js
+++ api/index.js
@@ -2,23 +2,8 @@ const express = require("express");
 const app = express();
 const fs = require("fs").promises;
 const path = require('path');
-//const dataPath = './data/data.json';
 const dataFilePath = path.join(__dirname, '../data/data.json');
 
-/*
-app.get("/express", (req, res) => {
-  fs.readFile(dataPath, "utf8", (err, data) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send("Server Error");
-    } else {
-      res.json(JSON.parse(data));
-    }
-  });
-});
-*/
-
-// Define a route to serve the JSON file
 app.get('/express', async (req, res) => {
     try {
       const data = await fs.readFile(dataFilePath, 'utf8');
@@ -30,10 +15,7 @@ app.get('/express', async (req, res) => {
     }
   });
   
-
-//app.get("/cliente_servidor", (req, res) => res.send("Cliente Servidor on Vercel!"));
 app.use(express.static("public"));
-
 
 app.listen(3001, () => console.log("Server ready on port 3001."));
 
