5ed1e6ab3394c1b3dee14fc914433c50f0953633
Mario Córdoba <cord0388@gmail.com>
2024-03-31 07:37:48 -0300

    alternative for error

--- api/index.js
+++ api/index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const app = express();
 const fs = require("fs");
+const path = require('path');
 const dataPath = './data/data.json';
+//const dataPath = path.resolve(__dirname, '/data/data.json');
 
 app.get("/express", (req, res) => {
     fs.readFile(dataPath, 'utf8', (err, data) => {
         if (err) {
             console.error(err);
-            res.status(500).send('Server Error');
+            res.status(500).send('Server Error' + ' Path: ' + dataPath);
         } else {
             res.json(JSON.parse(data));
         }
